b283d62d05dd0e0c303bb8b40f953126381ede95
Endale-Temesgen <endutem2015@gmail.com>
2022-12-10 01:57:43 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-# include <Stdio.h>
+#include <stdio.h>
   /**
-  * main -Entry
-  * Return: Always 0
-  */
- int.main(void)
- {
+   *
+  
+   * Return: Always 0 (Success)
+   */
+ 
+int main(void)
+
+{
+      
     puts("\"programming is like building a multilingual puzzle");
 
                  return(0);
