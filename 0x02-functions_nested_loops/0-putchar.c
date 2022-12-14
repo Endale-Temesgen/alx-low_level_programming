3aaeed258c86b1d14df243bafafd411a9813ac2b
Endale-Temesgen <endutem2015@gmail.com>
2022-12-14 02:13:25 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main - Entrypoint
  * Return: Always 0
  */
 int main(void)
@@ -15,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
-	return (0);
+        return (0);
 }
